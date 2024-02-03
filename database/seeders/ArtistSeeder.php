1a428f75f6ff29142287012b334af0a4b4cea49b
zakariya <zamyai@gmail.com>
2024-02-03 20:19:36 +0100

    Modèle Artist +migration +seeders

---
+++ database/seeders/ArtistSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Models\Artist;
+
+class ArtistSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Empty the table first
+        Artist::truncate();
+  //Define data
+       $artists = [
+            ['firstname'=>'Daniel','lastname'=>'Marcelin'],
+            ['firstname'=>'Philippe','lastname'=>'Laurent'],
+            ['firstname'=>'Marius','lastname'=>'Von Mayenburg'],
+            ['firstname'=>'Olivier','lastname'=>'Boudon'],
+            ['firstname'=>'Anne Marie','lastname'=>'Loop'],
+            ['firstname'=>'Pietro','lastname'=>'Varasso'],
+            ['firstname'=>'Laurent','lastname'=>'Caron'],
+            ['firstname'=>'Élena','lastname'=>'Perez'],
+            ['firstname'=>'Guillaume','lastname'=>'Alexandre'],
+            ['firstname'=>'Claude','lastname'=>'Semal'],
+            ['firstname'=>'Laurence','lastname'=>'Warin'],
+        ];
+         //Insert data in the table
+         DB::table('artists')->insert($artists);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,10 @@ class DatabaseSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
+
+    {$this->call([
+            ArtistSeeder::class,
+        ]);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
