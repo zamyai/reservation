1105e05e1d2af8681a9f79d7cc7c823701e6a9ac
zakariya <zamyai@gmail.com>
2024-02-08 09:50:27 +0100

    Controller Artist +templates +routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArtistController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/artist', [ArtistController::class, 'index'])->name('artist.index');
+Route::get('/artist/{id}', [ArtistController::class, 'show'])
+		->where('id', '[0-9]+')->name('artist.show');
+
